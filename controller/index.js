fefb75057b9d06563f69738e3d1227c8e8c96ea1
m-vd <mikvadi.9@gmail.com>
2018-11-28 09:47:07 +0700

    fixes error

--- controller/index.js
+++ controller/index.js
@@ -2,10 +2,10 @@ var express = require('express'),
     router = express.Router();
 
 
-Router.get("/", function(req, res){
+router.get("/", function(req, res){
     res.render("index");
 });
 
-Router.get("/about", function(req, res){
+router.get("/about", function(req, res){
     res.render("about");
 })
