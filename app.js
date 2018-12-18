cec29b703042a065f4aa438fc84c2260b2ba0c8b
nafisahnurul <Nafisah.nurul15@gmail.com>
2018-12-18 09:31:55 +0700

    refresh

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ var express = require('express'),
     LocalStrategy = require('passport-local'),
     passport = require('passport');
 
-var Psychologist = require('./models/psychologist');
+var Psychologist = require('./models/psychologist'),
+    User = require('./models/user');
 
-var authPsyRoutes = require('./controller/authpsy'),
+var userAuth = require('./controller/userAuth')
+    authPsyRoutes = require('./controller/authpsy'),
     stdRoutes = require('./controller/student'),
     scheduleRoutes= require('./controller/schedule'),
     indexRoutes   = require('./controller/index');
 
 app.use(cookieParser());
 app.use(require("express-session")({
-    key: "user_sid",
-    secret: "II3120 - IT Services",
+    key: "user_researchapi",
+    secret: "Researchapilocalgalocalharusnyabisa",
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -33,20 +35,21 @@ passport.deserializeUser(Psychologist.deserializeUser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/views"));
 app.set("view engine", "ejs");
-mongoose.connect(process.env.database_uri || "mongodb://localhost/lbk", { useNewUrlParser: true });
+mongoose.connect(process.env.database_uri || "mongodb://localhost/researchapi", { useNewUrlParser: true });
 
 app.get("/", function (req, res) {
     res.render("index", { account: "" });
 });
 
 app.use("/", indexRoutes);
+app.use("/userAuth", userAuth);
 app.use("/student", stdRoutes);
 app.use("/psychologist", authPsyRoutes);
 app.use("/schedule", scheduleRoutes);
 
 //LOGIN AND AUTH FOR PSYCHOLOGISTS
 
-app.listen(process.env.PORT || 3120, function (req, res) {
+app.listen(process.env.PORT || 2000, function (req, res) {
     console.log("App is running");
-    console.log("Database running: ", (process.env.database_uri || "mongodb://localhost/lbk"))
+    console.log("Database running: ", (process.env.database_uri || "mongodb://localhost/researchapi"))
 })
