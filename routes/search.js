176b5866557b3e54731f7c59b0a51e3ade2d6082
nafisahnurul <nafisah.nurul15@gmail.com>
2018-12-19 20:08:33 +0700

    requesting data to other website

--- routes/search.js
+++ routes/search.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    request = require('request'),
     router = express.Router();
 
 router.get("/",function (req,res) {
@@ -7,6 +8,11 @@ router.get("/",function (req,res) {
 
 router.post("/result", function(req,res){
     let keyword = req.body.keyword;
+    let url = 'http://www.pathwaycommons.org/pc/webservice.do?version=2.0&q='+keyword+'&format=xml&cmd=search';
     console.log(keyword);
+    console.log(url);
+    request(url,function(error, response, body){
+        console.log(body);
+    })
 })
 module.exports = router;
