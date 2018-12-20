d8f6c80030f96202d26f353c17e654a1400b5bf3
nafisahnurul <nafisah.nurul15@gmail.com>
2018-12-20 17:11:50 +0700

    removing unnecessary line

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
 const path = require('path');
 const app = express();
 
- const getHomePage = require('./routes/index');
- const getAuth = require('./routes/google-util');
- const getSearch = require('./routes/search');
+const getHomePage = require('./routes/index');
+const getAuth = require('./routes/google-util');
+const getSearch = require('./routes/search');
 const port = 5000;
-
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'socka'
-});
-
-// connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
-});
-global.db = db;
 
 // configure middleware
 app.set('port', process.env.port || port); // set express to use this port
